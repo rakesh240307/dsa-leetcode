a2519634c0473a6f830abd1713ccec8976000ee9
rakesh240307 <rakesh.24scse1380003@galgotiasuniversity.ac.in>
2026-06-16 22:53:54 +0530

    Time: 0 ms (100%), Space: 42.4 MB (76.6%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,18 +1,11 @@
 class Solution {
     public int countCommas(int n) {
         
-
-        int ans = 0;
-        int p = 1000; // 10^3
-
-        while (p <= n) {
-            ans += (n - p + 1);
-            
-            if (p >Integer.MAX_VALUE / 1000) break;
-            p *= 1000;
+        int a = n-999;
+        
+        if(a<0){
+            return 0;
         }
-
-        return ans;
+        return a;
     }
 }
-    
