1c58ad9393d2353444cd3d2659a4e54a76270096
rakesh240307 <rakesh.24scse1380003@galgotiasuniversity.ac.in>
2026-08-01 00:42:30 +0530

    Time: 26 ms (48.27%), Space: 47.2 MB (13.37%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        for(int i=0;i<s.length();i++){
+
+    char ch = s.charAt(i);
+
+    if(s.indexOf(ch) == s.lastIndexOf(ch))
+        return i;
+}
+
+return -1;
+        
+    }
+}
