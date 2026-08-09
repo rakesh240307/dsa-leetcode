7c5a10e38f4a0857c442b23dd71d6af18a81b079
rakesh240307 <rakesh.24scse1380003@galgotiasuniversity.ac.in>
2026-08-09 21:40:07 +0530

    Time: 2 ms (99.29%), Space: 46.7 MB (50.06%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,36 +1,38 @@
 class Solution {
-    public String reverseVowels(String s) {
-
-        char[] arr = s.toCharArray();
-
-        int i = 0;
-        int j = arr.length - 1;
-
-        while (i < j) {
-
-            while (i < j && !isVowel(arr[i])) {
-                i++;
-            }
-
-            while (i < j && !isVowel(arr[j])) {
-                j--;
-            }
-
-            char temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-
-            i++;
-            j--;
-        }
-
-        return new String(arr);
-    }
-
-    public boolean isVowel(char ch) {
-        return ch == 'a' || ch == 'e' || ch == 'i' ||
+    public boolean v(char ch){
+        boolean a = false;
+        if(ch == 'a' || ch == 'e' || ch == 'i' ||
                ch == 'o' || ch == 'u' ||
                ch == 'A' || ch == 'E' || ch == 'I' ||
-               ch == 'O' || ch == 'U';
+               ch == 'O' || ch == 'U'){
+                a = true;
+               }
+               return a ;
+    }
+    public String reverseVowels(String s) {
+        int i = 0 ;
+        int j = s.length()-1;
+        char[] arr = s.toCharArray();
+        while(i<=j){
+            if(!v(s.charAt(i))){
+                i++;
+            }
+            else if(!v(s.charAt(j))){
+                j--;
+            }
+            else{
+                char temp = arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                i++;
+                j--;
+
+            }
+                
+            
+        }
+        return new String(arr);
+
+        
     }
 }
