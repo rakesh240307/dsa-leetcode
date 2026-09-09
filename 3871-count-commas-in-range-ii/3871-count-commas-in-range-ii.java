c6cf566027ffb412e1bd77adbced20e0419163fb
rakesh240307 <rakesh.24scse1380003@galgotiasuniversity.ac.in>
2026-09-09 12:12:51 +0530

    Time: 1 ms (99.36%), Space: 42.7 MB (37.18%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long countCommas(long n) {
+
+        long ans = 0;
+        long p = 1000; // 10^3
+
+        while (p <= n) {
+            ans += (n - p + 1);
+            
+            if (p > Long.MAX_VALUE / 1000) break;
+            p *= 1000;
+        }
+
+        return ans;
+    }
+}
